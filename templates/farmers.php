7400b4aeafbc1a1dfd0fb679ca88c84f4186be96
kdarshan425 <64018424+kdarshan425@users.noreply.github.com>
2021-08-21 20:39:33 +0530

    Update farmers.php

--- templates/farmers.php
+++ templates/farmers.php
@@ -142,7 +142,13 @@
             </div>
         </div>
     </div>
-    
+    <div style="padding-top:20px;">
+        <footer style="background: black;padding-top: 24px;padding-bottom: 24px; width: 100%">
+            <center>
+                <p style="color: white;font-weight: 300;font-family: 'Raleway', sans-serif;">All rights reserved <br> © Team Cropit</p>
+            </center>            
+        </footer>
+    </div>
     <script>
      setTimeout(function () {
   
